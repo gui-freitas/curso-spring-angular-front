d5b042905b3f7133db866c0c816a224f639ccae9
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-10-25 19:12:28 -0300

    Finalizacao template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
