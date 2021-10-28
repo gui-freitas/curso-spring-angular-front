a89948aa526dc562c034bcd19057623a2af6159b
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-10-28 16:42:39 -0300

    Criacao do componente de listagem de Clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -15,11 +15,16 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getCliente() : Cliente {
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '01234567890';
-    return cliente;
+  //getClientes() : Observable<Cliente[]> {
+//    return null;
+  //}
+  getClientes() : Cliente[]{
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano destes';
+    cliente.dataCadastro = '18/04/2020';
+    cliente.cpf = '091234567890';
+    return [cliente];
   }
 
 }
