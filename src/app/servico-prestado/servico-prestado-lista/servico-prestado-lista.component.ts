04f0434564e6243ddf757237179bd158faf44cf9
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-11-08 18:32:09 -0300

    Criacao formulario de pesquisa Servicos Prestados

--- src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoService } from '../../servico-prestado.service';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoListaComponent implements OnInit {
 
-  constructor() { }
+  nome: string;
+  mes: number;
+  meses: number[];
+  listaServicoPrestado: ServicoPrestadoBusca[];
+  mensagem: string;
+
+  constructor(
+    private servicePrestadoService: ServicoPrestadoService
+  ) {
+    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12];
+   }
 
   ngOnInit(): void {
   }
 
+  consultar(){
+    this.servicePrestadoService
+    .consultar(this.nome, this.mes)
+    .subscribe(response => {
+      this.listaServicoPrestado = response;
+      if(this.listaServicoPrestado.length <= 0){
+        this.mensagem = "Nenhum registro encontrado.";
+      }else{
+        this.mensagem = "";
+      }
+    });
+  }
 }
---
+++ src/app/servico-prestado/servico-prestado-lista/servicoPrestadoBusca.ts
@@ -0,0 +1,8 @@
+import { Cliente } from "../../clientes/cliente";
+
+export class ServicoPrestadoBusca{
+    descricao:string;
+    valor: number;
+    dataExecucao: string;
+    cliente: Cliente;
+}
